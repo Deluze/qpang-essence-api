49bad58de97074a545eeb29301d301530a2f4591
Deluze <bartramaekers99@gmail.com>
2020-05-28 09:10:21 +0200

    Construct player skill card entry on player create

--- app/Listeners/OnPlayerCreated.php
+++ app/Listeners/OnPlayerCreated.php
@@ -3,13 +3,14 @@
 namespace App\Listeners;
 
 use App\Events\PlayerCreated;
+use App\Models\PlayerSkillCards;
 
 class OnPlayerCreated
 {
     /**
      * Handle the event.
      *
-     * @param \App\Events\PlayerCreated $playerCreated
+     * @param  \App\Events\PlayerCreated $event
      * @return void
      */
     public function handle(PlayerCreated $event)
@@ -39,6 +40,10 @@ class OnPlayerCreated
 
         $player->equipment()->create([
             'character_id' => 851,
+        ]);
+
+        PlayerSkillCards::query()->create([
+            'player_id' => $player->getKey(),
         ]);
     }
 }
